cb647a9c399c9e38049ef4f80af77b1daffc484c
jaewooKim <108774881+wyswhsl21@users.noreply.github.com>
2023-04-16 23:56:36 +0900

    [level 1] Title: 내적, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    var answer = [];
+    for (let i=0 ; i < a.length; i++){
+    answer.push(a[i]*b[i])
+    }
+    
+    return answer.reduce((acc,cur)=>acc+cur,0)
+    
+    
+    
+    
+    
+}
